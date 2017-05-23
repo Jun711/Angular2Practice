6b1f46294a71d1ddf072f6dffb21a7110a19e1a6
--get-all <cygoh@umich.edu>
2017-05-22 23:59:26 -0700

    Depency injection

---
+++ src/app/shared/collectable.service.ts
@@ -0,0 +1,15 @@
+import { Collectable } from "./collectable.model";
+
+export class CollectableService {
+	private collectables: Collectable[] = [
+		{description: 'A very rare copy of "jQuery for Dummies"', type: 'Book'},
+	    {description: 'The first Letter ever written', type: 'Piece of Paper'},
+	    {description: 'A photograph showing nothing', type: 'Photo'},
+	    {description: 'A box with all sold Zunes', type: 'Garbage'}
+	];
+
+	getCollectibles() {
+		return this.collectables;
+	}
+
+}
